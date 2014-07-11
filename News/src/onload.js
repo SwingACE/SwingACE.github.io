a36ad76718dc0741dced89c792cf83531a2713d1
SwingACE <bogeliu@126.com>
2014-07-12 00:16:51 +0800

    图片轮播2

---
+++ News/src/onload.js
@@ -0,0 +1,30 @@
+window.onload=function(){
+	loadComments(pageList[pageIndex]);
+	document.getElementById("pageUp").addEventListener("click",pageUp);
+	document.getElementById("pageDown").addEventListener("click",pageDown);
+	document.getElementById("slideRight").addEventListener("click",function(){
+		if(index < list.length-1){
+			index++;
+			loadPhoto(index);
+			time = new Date;
+		}
+		else if(index == list.length-1){
+			index = 0;
+			loadPhoto(index);
+			time = new Date;
+		}
+	});
+	document.getElementById("slideLeft").addEventListener("click",function(){
+		if(index > 0){
+			index--;
+			loadPhoto(index);
+			time = new Date;
+		}
+		else if(index == 0){
+			index = list.length-1;
+			loadPhoto(index);
+			time = new Date;
+		}
+	});
+	$(".showWindow img").hover(function(){clearInterval(timer);},function(){time = new Date;timer = setInterval(updateNewsAuto);});
+}
